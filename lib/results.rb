a9669797aef6ce6df4c699c135f6da51e05b8ca5
sts10 <sschlinkert@gmail.com>
2014-02-23 18:20:56 -0500

    working struct

--- lib/results.rb
+++ lib/results.rb
@@ -19,20 +19,25 @@ class Results
     return `grep -r -n --include=*.rb '#{@query}' /Users/samschlinkert/Documents/code/flatiron`
   end
 
+  Result = Struct.new(:file_path, :line_number, :code_snippet) 
 
-  Result = Struct.new(:file_path, :line_number, :code_snippet) do
-    def parse_results
-      results = get_grep_results.split("\n")
-    
-    
-      array_of_arrays = results.map do |result|
-        line_array = []
-        line_array = result.split(":")
-        Result.new(line_array[0], line_array[1], line_array[2])
-        binding.pry 
-      end 
-    end
+  def parse_results
+    results = get_grep_results.split("\n")
+  
+    array_of_result_structs = []
+  
+    results.each do |result|
+      line_array = []
+      line_array = result.split(":")
+      this_result = Result.new(line_array[0], line_array[1], line_array[2])
+      array_of_result_structs << this_result
+    end 
+
+    return array_of_result_structs
+
   end
+
+  
 
  
 end
