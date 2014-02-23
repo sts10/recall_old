67ae26cd3ff8e5e8b8682e58192feef2fd45036c
sts10 <sschlinkert@gmail.com>
2014-02-23 18:07:35 -0500

    struct is in but it doesn't work

--- lib/results.rb
+++ lib/results.rb
@@ -19,18 +19,20 @@ class Results
     return `grep -r -n --include=*.rb '#{@query}' /Users/samschlinkert/Documents/code/flatiron`
   end
 
-  def parse_results
-    results = get_grep_results.split("\n")
-    
-    
-    array_of_arrays = results.map do |result|
-      line_array = []
-      line_array = result.split(":")
- 
-    end 
-  end
 
- 
+  Result = Struct.new(:file_path, :line_number, :code_snippet) do
+    def parse_results
+      results = get_grep_results.split("\n")
+    
+    
+      array_of_arrays = results.map do |result|
+        line_array = []
+        line_array = result.split(":")
+        Result.new(line_array[0], line_array[1], line_array[2])
+        binding.pry 
+      end 
+    end
+  end
 
  
 end
